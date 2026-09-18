ec7bc1af9b245dfd5939e0b5c7b910e39e7122d1
Brian Ainscough <brianainscoughwork@gmail.com>
2026-09-18 14:07:25 +0100

    configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import preact from '@preact/preset-vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [preact()],
   base: 'https://github.com/brianainscough/uktv-starwars',
 })
